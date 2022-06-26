38434cb996cda6384282338af97ece80b243d890
Ariel Maldonado <arielsebas2000@gmail.com>
2022-06-26 18:55:29 -0500

    Tarea 12: Configuracion log4j.

--- src/main/java/com/uce/edu/demo/ProyectoU2AmApplication.java
+++ src/main/java/com/uce/edu/demo/ProyectoU2AmApplication.java
@@ -1,11 +1,15 @@
 package com.uce.edu.demo;
 
+import org.apache.log4j.Logger;
+
 import org.springframework.boot.CommandLineRunner;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
 public class ProyectoU2AmApplication implements CommandLineRunner {
+
+	private static Logger logger = Logger.getLogger(ProyectoU2AmApplication.class);
 
 	public static void main(String[] args) {
 		SpringApplication.run(ProyectoU2AmApplication.class, args);
@@ -14,7 +18,7 @@ public class ProyectoU2AmApplication implements CommandLineRunner {
 	@Override
 	public void run(String... args) throws Exception {
 		// TODO Auto-generated method stub
-		System.out.println("Hola Mundo.");
+		logger.debug("Hola Mundo.");
 	}
 
 }
